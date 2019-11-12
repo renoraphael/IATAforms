f308c8be99951ff7af4f0105366451bcf6ab37d0
Reno <renoraphael@gmail.com>
2019-11-12 12:51:21 +0800

    booking.py

--- booking.py
+++ booking.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, url_for, flash, redirect
 from flask_sqlalchemy import SQLAlchemy
-# from forms import AirportsForm
+from forms import AirportsForm
 # from pyairports import airports
 
 app = Flask(__name__)
